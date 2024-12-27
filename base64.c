109a9757895479d2f8d62d40912be87871b7b8f6
testation <>
2024-12-27 22:07:32 +0100

    Add base64Decode and fix some problem with meomry leaks and overflow

--- base64.c
+++ base64.c
@@ -15,46 +15,38 @@ const char *base64Encode(const char *input) {
   int *binary = (int *)calloc(size, sizeof(int));
 
   int *it = binary;
-  const char *inputIt = input;
 
-  while (*inputIt != '\0') {
-    for (int i = 7; i >= 0; --i) {
-      if (*inputIt & (1 << i)) {
+  for (int i = 0; i < strlen(input); ++i) {
+    for (int j = 7; j >= 0; --j) {
+      if (input[i] & (1 << j)) {
         *it = 1;
       }
       ++it;
     }
-    ++inputIt;
   }
 
   int roundedSize = round(size / 6) + (round(size / 6) - 4);
-  char *byteBase64 = (char *)malloc(roundedSize + 1);
+  char *byteBase64 = (char *)calloc(roundedSize + 1, sizeof(char));
   memset(byteBase64, '=', roundedSize);
-  byteBase64[roundedSize + 1] = '\0';
 
-  it = binary;
-  char *base64 = byteBase64;
   int index = 0;
 
-  while (*base64 != '\0') {
+  for (int i = 0; i < strlen(byteBase64); ++i) {
     if (index == size) {
       break;
     }
     int sumOfBit = 0;
 
-    for (int i = 0; i < 6; ++i) {
-      if (*it == 1) {
-        sumOfBit += pow(2, 6 - 1 - i);
+    for (int j = 0; j < 6; ++j) {
+      if (binary[index] == 1) {
+        sumOfBit += pow(2, 6 - 1 - j);
       }
       ++index;
-      ++it;
     }
 
-    *base64 = baseAlphabet[sumOfBit];
-    ++base64;
+    byteBase64[i] = baseAlphabet[sumOfBit];
   }
 
-  free(it);
   free(binary);
 
   int padding = round((double)size / 6);
@@ -63,14 +55,78 @@ const char *base64Encode(const char *input) {
     return byteBase64;
   }
 
-  base64 = byteBase64;
-  base64 += padding;
-  while (*base64 != '\0') {
-    *base64 = '=';
-    ++base64;
+  for (int i = padding; i < strlen(byteBase64); ++i) {
+    byteBase64[i] = '=';
   }
 
-  free(base64);
-
   return byteBase64;
+}
+
+const char *base64Decode(const char *input) {
+  int len = 0;
+
+  for (; len < strlen(input); ++len) {
+    if (input[len] == '=') {
+      break;
+    }
+  }
+
+  char *resizedInput = (char *)calloc(len + 1, sizeof(char));
+  int *binary = (int *)calloc(len * 6, sizeof(int));
+
+  for (int i = 0; i < len; ++i) {
+    resizedInput[i] = input[i];
+  }
+
+  int *binaryIt = binary;
+
+  for (int i = 0; i < strlen(resizedInput); ++i) {
+    int data = 0;
+    if ('A' <= resizedInput[i] || resizedInput[i] <= 'Z') {
+      data = resizedInput[i] - 0x41;
+    } else if ('a' <= resizedInput[i] || resizedInput[i] <= 'z') {
+      data = resizedInput[i] - 0x61 + 26;
+    } else if ('0' <= resizedInput[i] || resizedInput[i] <= '9') {
+      data = resizedInput[i] - 0x61 + 52;
+    } else if (resizedInput[i] == '+') {
+      data = 62;
+    } else if (resizedInput[i] == '/') {
+      data = 63;
+    }
+
+    for (int i = 0; i < 6; ++i) {
+      int pow2 = (int)pow(2, 6 - 1 - i);
+      if (data - pow2 >= 0) {
+        data -= pow2;
+        *binaryIt = 1;
+      } else {
+        *binaryIt = 0;
+      }
+      ++binaryIt;
+    }
+  }
+
+  free(resizedInput);
+
+  binaryIt = binary;
+
+  int size = len * 6 % 8;
+  char *result = (char *)calloc(size + 1, sizeof(char));
+
+  for (int i = 0; i < size; ++i) {
+    int calcResult = 0;
+
+    for (int j = 0; j < 8; ++j) {
+      if (*binaryIt == 1) {
+        calcResult += pow(2, 8 - 1 - j);
+      }
+      ++binaryIt;
+    }
+
+    result[i] = (char)calcResult;
+  }
+
+  free(binary);
+
+  return result;
 }
--- base64.h
+++ base64.h
@@ -3,4 +3,6 @@
 
 const char *base64Encode(const char *input);
 
+const char *base64Decode(const char *input);
+
 #endif // BASE64_HH
