9314f21a058570b47b69fc92b7a66f3840917b64
testation <>
2024-12-28 01:46:26 +0100

    Rename the name of the macro in header file

--- base64.h
+++ base64.h
@@ -1,8 +1,8 @@
-#ifndef BASE64_HH
-#define BASE64_HH
+#ifndef BASE64_H
+#define BASE64_H
 
 const char *base64Encode(const char *input);
 
 const char *base64Decode(const char *input);
 
-#endif // BASE64_HH
+#endif // BASE64_H
